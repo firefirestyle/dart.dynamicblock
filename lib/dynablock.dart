b4e28b5239e836cd783d76d841a4dd318325ccef
kyorohiro <kyorohiro@gmail.com>
2016-10-25 07:42:44 +0900

    mod function and object name

--- lib/dynablock.dart
+++ lib/dynablock.dart
@@ -2,18 +2,46 @@ library dynablock;
 
 class DynaBlockCore {
   String rootID;
-  List<DynaBlockItem> infos = [];
+  List<FreeSpaceInfo> infos = [];
   int rootWidth = 0;
   int rootHeight = 0;
 
   DynaBlockCore({this.rootWidth: 400}) {
-    infos.add(new DynaBlockItem()
+    infos.add(new FreeSpaceInfo()
       ..xs = 0
       ..y = 10
       ..xe = rootWidth);
   }
 
-  addInfo(int xs, int ys, int xe, int ye) {
+  FreeSpaceInfo addBlock(int elmW, int elmH) {
+    print("\r\n\r\n#===> ###");
+    //
+    //
+    FreeSpaceInfo info = null;
+    for (int i = 0; i < infos.length; i++) {
+      info = infos[i];
+      var w = (info.xe - info.xs);
+      if (w >= elmW) {
+        print("======SELECT>${info.toString()}");
+        break;
+      }
+      info = null;
+    }
+    if (info == null) {
+      // todo
+      return null;
+    }
+    //
+    updateIndex(info.xs, info.y, info.xs + elmW, info.y + elmH);
+
+    for (var j in infos) {
+      print(">>info:${j.toString()};");
+    }
+    return info;
+  }
+
+  
+  updateIndex(int xs, int ys, int xe, int ye) {
     print("##> addInfo ${xs}, ${ys}, ${xe}, ${ye}");
     //
     // [width]
@@ -29,15 +57,15 @@ class DynaBlockCore {
           // add
           print("##> in ${info.toString()}");
           if (added == false) {
-            var v = new DynaBlockItem.fromPos(xe, ys, info.xe - xe, xs, xe);
+            var v = new FreeSpaceInfo.fromPos(xe, ys, info.xe - xe, xs, xe);
             if (false == infos.contains(v)) {
               added = true;
               infos.add(v);
             }
           }
           {
-            var base1 = new DynaBlockItem.clone(info);
-            var base2 = new DynaBlockItem.clone(info);
+            var base1 = new FreeSpaceInfo.clone(info);
+            var base2 = new FreeSpaceInfo.clone(info);
             info.xs = info.xe = 0;
             base1.xe = xs;
             base2.xs = xe;
@@ -71,7 +99,7 @@ class DynaBlockCore {
     //
     // [height] over
     {
-      var head = new DynaBlockItem.fromPos(0, ye, rootWidth, xs, xe);
+      var head = new FreeSpaceInfo.fromPos(0, ye, rootWidth, xs, xe);
       print("##> heigh st ${head.toString()}");
       for (int i = 0; i < infos.length; i++) {
         var info = infos[i];
@@ -108,41 +136,16 @@ class DynaBlockCore {
     //
     // [sort] order high
     {
-      infos.sort((DynaBlockItem a, DynaBlockItem b) {
+      infos.sort((FreeSpaceInfo a, FreeSpaceInfo b) {
         return a.y - b.y;
       });
     }
   }
 
-  addA(int elmW, int elmH) async {
-    print("\r\n\r\n#===> ###");
-    //
-    //
-    DynaBlockItem info = null;
-    for (int i = 0; i < infos.length; i++) {
-      info = infos[i];
-      var w = (info.xe - info.xs);
-      if (w >= elmW) {
-        print("======SELECT>${info.toString()}");
-        break;
-      }
-      info = null;
-    }
-    if (info == null) {
-      // todo
-      return null;
-    }
-    //
-    addInfo(info.xs, info.y, info.xs + elmW, info.y + elmH);
 
-    for (var j in infos) {
-      print(">>info:${j.toString()};");
-    }
-    return info;
-  }
 }
 
-class DynaBlockItem {
+class FreeSpaceInfo {
   int xs;
   int y;
   int xe;
@@ -150,10 +153,10 @@ class DynaBlockItem {
   static int idid = -1;
   int baseXs;
   int baseXe;
-  DynaBlockItem() {
+  FreeSpaceInfo() {
     id = idid++;
   }
-  DynaBlockItem.clone(DynaBlockItem base) {
+  FreeSpaceInfo.clone(FreeSpaceInfo base) {
     this.xs = base.xs;
     this.y = base.y;
     this.xe = base.xe;
@@ -161,7 +164,7 @@ class DynaBlockItem {
     this.baseXe = base.baseXe;
     id = idid++;
   }
-  DynaBlockItem.fromPos(int x, int y, int width, this.baseXs, this.baseXe) {
+  FreeSpaceInfo.fromPos(int x, int y, int width, this.baseXs, this.baseXe) {
     print("##> fromPos ${x},${y},${width}");
     id = idid++;
     this.xs = x;
@@ -172,7 +175,7 @@ class DynaBlockItem {
     return """[${id}] xs:xe=${xs}px,${xe}px; ys:${y}; ${baseXs}: ${baseXe}""";
   }
 
-  bool operator ==(DynaBlockItem v) {
+  bool operator ==(FreeSpaceInfo v) {
     return (xs == v.xs) && (xe == v.xe); //&& (y == v.y);// &&(baseXs == v.baseXs) &&(baseXe == v.baseXe);
   }
 }
