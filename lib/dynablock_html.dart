229a3b9c410c9466d09162a19c6bb43fa3df47ad
kyorohiro <kyorohiro@gmail.com>
2016-10-25 12:23:44 +0900

    ..

--- lib/dynablock_html.dart
+++ lib/dynablock_html.dart
@@ -4,39 +4,23 @@ import 'dynablock.dart' as dynablock;
 import 'dart:html' as html;
 import 'dart:async';
 
-class DynaView {
+class DynaHtmlView {
   String rootId;
   dynablock.DynaBlockCore core = null;
-  DynaView({this.rootId: "fire-listcontainer"}){
+  DynaHtmlView({this.rootId: "fire-listcontainer"}){
   }
 
   updateSize() {
       var rootElm = html.document.body.querySelector("#${rootId}");
       core = new dynablock.DynaBlockCore(rootWidth: rootElm.clientWidth);
   }
-  Future addWithLoading(html.Element elm) async {
+
+  add(html.Element elm) {
     var rootElm = html.document.body.querySelector("#${rootId}");
     rootElm.style.position = "relative";
-    rootElm.children.add(elm);
-    //
-    html.ElementList<html.ImageElement> imgs = html.document.querySelectorAll("img");
-    Completer c = new Completer();
-    bool isA = false;
-    if (imgs.length > 0) {
-      imgs.onLoad.listen((ev) {
-        if (isA == false) {
-          _add(elm);
-          c.complete();
-          isA = true;
-        }
-      });
-    } else {
-      _add(elm);
-      c.complete();
+    if(false == rootElm.contains(elm)) {
+      rootElm.children.add(elm);
     }
-    return c.future;
-  }
-  _add(html.Element elm) {
     if(core == null) {
       updateSize();
     }
@@ -44,5 +28,27 @@ class DynaView {
     elm.style.position = "absolute";
     elm.style.left = "${info.xs}px";
     elm.style.top = "${info.y}px";
+  }
+}
+
+class DynaViewHelper {
+  //
+  // todo
+  Future<html.Element> waitByOnLoadImg(html.Element elm) async {
+    //
+    html.ElementList<html.ImageElement> imgs = html.document.querySelectorAll("img");
+    Completer c = new Completer();
+    bool isA = false;
+    if (imgs.length > 0) {
+      imgs.onLoad.listen((ev) {
+        if (isA == false) {
+          c.complete(elm);
+          isA = true;
+        }
+      });
+    } else {
+      c.complete(elm);
+    }
+    return c.future;
   }
 }
